b0fd8f7f408ea530f1f60646495f9167496d59c3
kristijan10048 <kristijan.petkov@gmail.com>
2018-01-03 22:04:52 +0100

    Initial commit

---
+++ CCharpLessons/Attributes/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Attributes
+{
+    class Program
+    {
+        [AttributeUsage(AttributeTargets.All)]
+        public class HelpAttribute : System.Attribute
+        {
+            public readonly string Url;
+
+            public string Topic               // Topic is a named parameter
+            {
+                get
+                {
+                    return topic;
+                }
+                set
+                {
+
+                    topic = value;
+                }
+            }
+
+            public HelpAttribute(string url)  // url is a positional parameter
+            {
+                this.Url = url;
+            }
+
+            private string topic;
+        }
+
+        [HelpAttribute("http://localhost/MyClassInfo")]
+        class MyClass
+        {
+        }
+
+        static void Main(string[] args)
+        {
+            //Accessing Attributes Through Reflection
+            System.Reflection.MemberInfo info = typeof(MyClass);
+            object[] attributes = info.GetCustomAttributes(true);
+            for (int i = 0; i < attributes.Length; i++)
+            {
+                System.Console.WriteLine(attributes[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
