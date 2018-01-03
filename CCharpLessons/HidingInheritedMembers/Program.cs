f61fc8c8be5b02e5b818f1b02a44b698545a3c79
kristijan10048 <kristijan.petkov@gmail.com>
2018-01-03 22:14:55 +0100

    Initial commit

---
+++ CCharpLessons/HidingInheritedMembers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HidingInheritedMembers
+{
+    class Program
+    {
+        public class A { public int Counter = 1; }
+
+        //hide Counter form Class A
+        public class B : A { public  new int Counter = 2; }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
