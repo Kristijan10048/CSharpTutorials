0ed607afcec195bb4cc7931fe8d981a0f0dfaa0d
kristijan10048 <kristijan.petkov@gmail.com>
2018-01-03 21:41:30 +0100

    Initial commit

---
+++ CCharpLessons/HelloWorld/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+
+            int suma = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 2 == 0)
+                    Console.WriteLine("Paren :{0}", i);
+
+                suma = suma + i;
+            }
+
+            Console.WriteLine("Suma: {0}", suma);
+            Console.ReadKey();
+        }
+    }
+}
