da198d10be8c228c28ebd6249a14de5e050444fa
kristijan10048 <kristijan.petkov@gmail.com>
2018-01-03 22:08:38 +0100

    Initial commit

---
+++ CCharpLessons/ConstructorsAndInheritance/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConstructorsAndInheritance
+{
+    public class Baseclass
+    {
+        public int X;
+        public Baseclass() { }
+        public Baseclass(int x) { this.X = x; }
+    }
+    public class Subclass : Baseclass { }
+
+    public class B
+    {
+        int x = 1; // Executes 3rd
+        public B(int x)
+        {
+            // Executes 4th
+        }
+    }
+    public class D : B
+    {
+        int y = 1; // Executes 1st
+        public D(int x)
+            : base(x + 1) // Executes 2nd
+        {
+            // Executes 5th
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
