056a4c644fdf1f4b5a6c981cced6319ad80ffc12
Kristijan <kristijan.petkov@gmail.com>
2026-07-10 13:49:45 +0200

    Crated a demo class with hybrid direcotry

--- CCharpLessons/Lesson19HybridDictionary/Program.cs
+++ CCharpLessons/Lesson19HybridDictionary/Program.cs
@@ -5,6 +5,60 @@ using System.Collections.Specialized;
 
 namespace Lesson19HybridDictionary
 {
+
+    public class HybridDictionaryDemo
+    {
+
+        private HybridDictionary m_hb;
+
+        #region Public Methods
+        public void HybridDictionaryExample()
+        {
+            m_hb = new HybridDictionary();
+        }
+
+        public bool HybridDictionaryAdd(string key, string value)
+        {
+            try
+            {
+                m_hb.Add(key, value);
+                return true;
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("An element with the same key already exists in the HybridDictionary.");
+                return false;
+            }
+        }
+
+        public object HybridDictionaryGet(string key)
+        {
+            return (string)m_hb[key];
+        }
+
+        public bool HybridDictionaryRemove(string key)
+        {
+            if (m_hb.Contains(key))
+            {
+                m_hb.Remove(key);
+
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("The key does not exist in the HybridDictionary.");
+                return false;
+            }
+        }
+
+        public System.Collections.ICollection getVales()
+        {
+            return m_hb.Values;
+        }
+        #endregion
+
+    }
+
     class Program
     {
         static void Main(string[] args)
