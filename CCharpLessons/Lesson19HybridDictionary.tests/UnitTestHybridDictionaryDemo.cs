0e86d43f03c60995f46f0abe8761e8e9020e3205
Kristijan <kristijan.petkov@gmail.com>
2026-07-12 13:55:14 +0200

    Extended tests

--- CCharpLessons/Lesson19HybridDictionary.tests/UnitTestHybridDictionaryDemo.cs
+++ CCharpLessons/Lesson19HybridDictionary.tests/UnitTestHybridDictionaryDemo.cs
@@ -6,7 +6,7 @@ namespace Lesson19HybridDictionary.tests
     public class UnitTestHybridDictionaryDemo
     {
         [Fact]
-        public void HybridDictionaryDemoTest1()
+        public void HybridDictionaryDemoAddTest()
         {
             HybridDictionaryDemo demo = new HybridDictionaryDemo();
 
@@ -14,11 +14,36 @@ namespace Lesson19HybridDictionary.tests
             demo.HybridDictionaryAdd("2", "2");
             demo.HybridDictionaryAdd("3", "3");
 
-           
-            Assert.Equal("1",  demo.HybridDictionaryGet("1") as string);
-            Assert.Equal("2",  demo.HybridDictionaryGet("2") as string);
-            Assert.Equal("3",  demo.HybridDictionaryGet("3") as string);
-            Assert.DoesNotContain("4",  demo.HybridDictionaryGet("4") as string);
+
+            Assert.Equal("1", demo.HybridDictionaryGet("1") as string);
+            Assert.Equal("2", demo.HybridDictionaryGet("2") as string);
+            Assert.Equal("3", demo.HybridDictionaryGet("3") as string);
+            Assert.DoesNotContain("4", demo.HybridDictionaryGet("4") as string);
+        }
+
+
+        [Fact]
+        public void HybridDictionaryDemeRemoveTest()
+        {
+            HybridDictionaryDemo demo = new HybridDictionaryDemo();
+
+            demo.HybridDictionaryAdd("1", "1");
+            demo.HybridDictionaryAdd("2", "2");
+            demo.HybridDictionaryAdd("3", "3");
+
+
+            Assert.Equal("1", demo.HybridDictionaryGet("1") as string);
+            Assert.Equal("2", demo.HybridDictionaryGet("2") as string);
+            Assert.Equal("3", demo.HybridDictionaryGet("3") as string);
+            Assert.DoesNotContain("4", demo.HybridDictionaryGet("4") as string);
+
+            demo.HybridDictionaryRemove("2");
+            Assert.DoesNotContain("2", demo.HybridDictionaryGet("2") as string);
             
+            demo.HybridDictionaryRemove("3");
+            Assert.DoesNotContain("3", demo.HybridDictionaryGet("3") as string);
+
+        }
+
     }
 }
