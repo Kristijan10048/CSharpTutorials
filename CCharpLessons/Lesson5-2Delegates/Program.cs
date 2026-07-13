27b310eb24846a488050ee6dbbaf7cdbc049f421
Kristijan <kristijan.petkov@gmail.com>
2026-07-13 19:30:46 +0200

    Introduced delegates examples

---
+++ CCharpLessons/Lesson5-2Delegates/Program.cs
@@ -0,0 +1,33 @@
+﻿public class DelegateDemo
+{
+    
+    public delegate void MyVoidDelegate();
+    public delegate int MyIntDelegate();
+    public delegate int MyIntDelegateWithParams(int a, int b);
+
+    public static void Main(string[] args)
+    {
+        //created a pointer to the HelloWorld method
+        MyVoidDelegate del = new MyVoidDelegate(HelloWorld);
+        del();
+
+        //created a pointer to the Add method. Returns an int value
+        MyIntDelegate del2 = new MyIntDelegate(getVal);
+
+        //created a pointer to the Add method. Returns an int value and takes two parameters
+        MyIntDelegateWithParams del3 = new MyIntDelegateWithParams(Add);
+
+        del3(1, 2);
+    }
+
+
+    public static int getVal()
+    { return 5; }
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public static void HelloWorld()
+    { }
+}
